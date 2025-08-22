c3b07f82f817e3c37ea547b08b624fc615f93154
Ashutosh9906 <ashutoshgandule1@gmail.com>
2025-08-22 18:31:07 +0530

    implemented error handling

--- controller/authenticate.js
+++ controller/authenticate.js
@@ -6,24 +6,34 @@ const oAuth2Client = require("../credentials");
 const TOKEN_PATH = path.join(__dirname, '..', 'json', 'token.json');
 
 async function handleAuthentication(req, res) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        prompt: "consent",
-        scope: ['https://www.googleapis.com/auth/gmail.readonly']
-    })
-    res.redirect(authUrl);
+    try {
+        const authUrl = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            prompt: "consent",
+            scope: ['https://www.googleapis.com/auth/gmail.readonly']
+        })
+        return res.redirect(authUrl);
+    } catch (error) {
+        console.log("Error", error);
+        return res.status(400).json({ err: "Internal Server Error" })
+    }
 }
 
 async function handleVerifyEmail(req, res) {
-    const code = req.query.code;
-    // const { tokens } = await oAuth2Client.getToken(code);
-    oAuth2Client.getToken(code).then(({ tokens }) => {
-        oAuth2Client.setCredentials(tokens);
-
-        fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
-        console.log("Tokens stored in token.json:", tokens);
-    });
-    return res.redirect("/messages");
+    try {
+        const code = req.query.code;
+        // const { tokens } = await oAuth2Client.getToken(code);
+        oAuth2Client.getToken(code).then(({ tokens }) => {
+            oAuth2Client.setCredentials(tokens);
+            
+            fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
+            console.log("Tokens stored in token.json:", tokens);
+        });
+        return res.redirect("/messages");
+    } catch (error) {
+        console.log("Error", error);
+        return res.status(400).json({ err: "Internal Server Error" })
+    }
 }
 
 module.exports = {
--- controller/message.js
+++ controller/message.js
@@ -13,20 +13,25 @@ if (fs.existsSync(TOKEN_PATH)) {
 }
 
 async function handleListMessage(req, res) {
-    const response = await gmail.users.messages.list({
-        userId: 'me',
-        labelIds: ['INBOX'],
-        maxResults: 5
-    })
+    try {
+        const response = await gmail.users.messages.list({
+            userId: 'me',
+            labelIds: ['INBOX'],
+            maxResults: 5
+        })
 
-    for (const msg of response.data.messages) {
-        const fullMessage = await getMessage(msg.id);
-        const parsed = parseMessage(fullMessage);
-        // parsed.body = parsed.body.replace(/https?:\/\/[^\s]+/g, "");
-        parsed.body = parsed.body.replace(/\s+/g, " ").trim();
-        console.log(parsed);
+        for (const msg of response.data.messages) {
+            const fullMessage = await getMessage(msg.id);
+            const parsed = parseMessage(fullMessage);
+            // parsed.body = parsed.body.replace(/https?:\/\/[^\s]+/g, "");
+            parsed.body = parsed.body.replace(/\s+/g, " ").trim();
+            console.log(parsed);
+        }
+        return res.status(200).json({ msg: "Messages parsed successfully" });
+    } catch (error) {
+        console.log("Error", error);
+        return res.status(400).json({ err: "Internal Server Error" })
     }
-    return res.status(200).json({ msg: "Messages parsed successfully" });
 }
 
 module.exports = {
