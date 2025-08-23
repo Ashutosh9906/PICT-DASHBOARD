852be656c6985401d43049bac0337539a2d0d907
Ashutosh9906 <ashutoshgandule1@gmail.com>
2025-08-23 10:37:03 +0530

    minor changes

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ config();
 
 const app = express();
 const oAuth2Client = require("./credentials");
-const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
 
 const authenticateRoute = require("./routes/authenticate");
 const messageRoute = require("./routes/messages");
-
-const SCOPES = process.env.SCOPES;
 
 //To authenticate User
 app.use("/user", authenticateRoute);
